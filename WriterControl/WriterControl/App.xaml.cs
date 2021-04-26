771b279c45bff19a7fe1e8f66ff0ad809ea7d571
Micrafast <1191069521@qq.com>
2021-04-26 13:15:55 +0800

    Added WriterControl

---
+++ WriterControl/WriterControl/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WriterControl
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            UIUtils.GetParentObject<Window>((DependencyObject)sender).Close();
+        }
+
+        private void MinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            UIUtils.GetParentObject<Window>((DependencyObject)sender).WindowState = WindowState.Minimized;
+        }
+
+        private void TitleBarMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                UIUtils.GetParentObject<Window>((DependencyObject)sender).DragMove();
+        }
+    }
+}
---
+++ WriterControl/WriterControl/UIUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WriterControl
+{
+    public class UIUtils
+    {
+        public static T GetParentObject<T>(DependencyObject obj) where T : DependencyObject
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+            while (parent != null)
+            {
+                if (parent is T)
+                {
+                    return (T)parent;
+                }
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return null;
+        }
+    }
+}
